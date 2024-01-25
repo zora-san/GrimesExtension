0d1ad01956dc07aa5c2c5c7ae6916d50a21c435c
Zora <137242094+zora-san@users.noreply.github.com>
2024-01-24 22:06:24 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,35 +1,20 @@
-function setupThumbnailRedirectListeners() {
-    let grimesPix = [
-        "http://bit.ly/3u824Ox",
-        "https://bit.ly/3StoRxJ",
-        "https://bit.ly/3UkDrsu",
-        "https://bit.ly/498xK5a",
-        "https://bit.ly/3vRe7An",
-        "https://bit.ly/4b6SM6b"
-    ]
+let grimesPix = [
+    "http://bit.ly/3u824Ox",
+    "https://bit.ly/3StoRxJ",
+    "https://bit.ly/3UkDrsu",
+    "https://bit.ly/498xK5a",
+    "https://bit.ly/3vRe7An",
+    "https://bit.ly/4b6SM6b"
+];
 
-    const index = Math.floor(Math.random() * grimesPix.length);
-    const grimesThumbnail = grimesPix[index];
-
-    chrome.declarativeNetRequest.updateDynamicRules({
-        addRules: [
-            {
-                "id": 1,
-                "priority": 1,
-                "action": {
-                    "type": "redirect",
-                    "redirect": {
-                        "regexSubstitution": grimesThumbnail
-                    }
-                },
-                "condition": {
-                    "regexFilter": "^https://i.ytimg.com/(vi|vi_webp)/(.*)/(default|hqdefault|mqdefault|sddefault|hq720)(_custom_[0-9]+)?.jpg(.*)",
-                    "resourceTypes": [
-                        "image"
-                    ]
-                }
-            }
-        ],
-        removeRuleIds: [1]
-    })
+function grimes() {
+    const pix = document.getElementsByTagName('img');
+    for (p of pix) {
+        if (!grimesPix.includes(p.src)) {
+            const index = Math.floor(Math.random() * grimesPix.length);
+            p.src = grimesPix[index];
+        }
+    }
 }
+
+setInterval(grimes, 5);
