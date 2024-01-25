b3927f9950719d583681298af494dc353b35ca52
Zora <137242094+zora-san@users.noreply.github.com>
2024-01-24 19:29:32 -0800

    Update content.js

--- content.js
+++ content.js
@@ -1,7 +1,10 @@
 let grimesPix = [
     "http://bit.ly/3u824Ox",
     "https://bit.ly/3StoRxJ",
-    "https://bit.ly/3UkDrsu"
+    "https://bit.ly/3UkDrsu",
+    "https://bit.ly/498xK5a",
+    "https://bit.ly/3vRe7An",
+    "https://bit.ly/4b6SM6b"
 ]
 
 const pix = document.getElementsByTagName('img');
