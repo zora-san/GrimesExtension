f5510d52acbfc82d429e9aebee569df5422079cc
Zora <137242094+zora-san@users.noreply.github.com>
2024-01-25 10:00:59 -0800

    Update content.js

--- content.js
+++ content.js
@@ -10,7 +10,7 @@ let grimesPix = [
 function grimes() {
     const pix = document.getElementsByTagName('img');
     for (p of pix) {
-        if (!grimesPix.includes(p.src)) {
+        if (!grimesPix.includes(pix.src)) {
             const index = Math.floor(Math.random() * grimesPix.length);
             p.src = grimesPix[index];
         }
